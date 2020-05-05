e97de297609303bd8356afdeb602ac18c9e36434
Mr. West <43521649+kwest10000@users.noreply.github.com>
2020-05-05 08:14:39 -0700

    Create get-tags-zones.py

---
+++ get-tags-zones.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+
+
+response = requests.get(f"https://192.168.55.10/restapi/v9.1/Policies/SecurityRules?location=vsys&vsys=vsys1",verify=False, 
+    headers={"X-PAN-KEY": "LUFRPT0yWGUvMlRxNGVtdXJ4YVNHSVFxL2Z3ZFArYnM9Y2NOdGxCM01PZEhRcFhZem94MXhzOXVNS2hkUW1tRDc1eEJ1NS91V1pieTkxSEIvK3hweWNza3pyTW5UM1lSMA=="
+    }
+)
+
+tag       = "Transit"
+from_zone = "Private"
+to_zone   = "Public"
+
+# convert json to Python object 
+data = response.json()
+
+#for loop to iterate through the tag variable and print the security rule that matches the tag
+print(f"\n#### TAG NAME: {tag} ####\n")
+for stuff in data['result']['entry']:
+    for morestuff in stuff['tag']['member']:
+        if (morestuff == tag):
+            #print(morestuff)
+            print(f"Security Rule Name with Tag \"{tag}\": {stuff['@name']}")
+
+#for loop to iterate through the "from_zone" variable and print the security rule that matches that zone
+print(f"\n#### SOURCE ZONE: {from_zone} ####\n")
+for stuff in data['result']['entry']:
+    for morestuff in stuff['from']['member']:
+        if (morestuff == from_zone):
+            #print(morestuff)
+            print(f"Security Rule Name with source zone \"{from_zone}\": {stuff['@name']}")
+
+#for loop to iterate through the "to_zone" variable and print the security rule that matches that zone
+print(f"\n#### DEST ZONE: {to_zone} ####\n")
+for stuff in data['result']['entry']:
+    for morestuff in stuff['to']['member']:
+        if (morestuff == to_zone):
+            #print(morestuff)
+            print(f"Security Rule Name with destination zone \"{to_zone}\": {stuff['@name']}")
+
